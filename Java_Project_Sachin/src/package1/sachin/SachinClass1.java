563ce6179e5306afb40e8c695783e770b221a019
Sachin <Sachin@LAPTOP-3GS9JF6M>
2019-03-17 19:37:41 +0000

    try

---
+++ Java_Project_Sachin/src/package1/sachin/SachinClass1.java
@@ -0,0 +1,42 @@
+package package1.sachin;
+
+public class SachinClass1 {
+
+		 private int a ;
+		 private int b ;
+		 private int c ;
+		 
+		
+		  public void setVala (int apass)
+		  {
+		  a = apass;
+		  }
+		  
+		  public int getVala ()
+		  {
+		  return a;
+		  }
+		    
+		  public void setValb (int bpass)
+		  {
+		  b = bpass;
+		  }
+		  
+		  public int getValb ()
+		  {
+		  return b;
+		  }
+		    
+		  public void setValc (int cpass)
+		  {
+		  c = cpass;
+		  }
+		  
+		  public int getValc ()
+		  {
+		  return c;
+		  }  
+		  
+		  
+		}
+//}
---
+++ Java_Project_Sachin/src/package2/sachin/SachinClass2.java
@@ -0,0 +1,20 @@
+package package2.sachin;
+import package1.sachin.SachinClass1;
+public class SachinClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SachinClass1 s1 = new SachinClass1();
+		s1.setVala(30);
+		s1.setValb(25);
+				int n1 = s1.getVala();
+				int n2 = s1.getValb();
+				int n3 = s1.getValc();
+			n3 = n1 + n2;
+			System.out.println("results = " + n3 );
+		
+			
+			
+	}
+
+}
